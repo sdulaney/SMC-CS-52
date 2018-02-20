e9b415e793520ed93576c13b9407b174f7c203da
Stewart Dulaney <sdulaney@users.noreply.github.com>
2018-02-20 10:59:10 -0800

    add solution for problem 1

--- Assignment1/Assignment1/Problem1.cpp
+++ Assignment1/Assignment1/Problem1.cpp
@@ -2,12 +2,17 @@
 // Assignment 1
 // Problem 1
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    
+    cout << "Hello, World\n";
+    string name;
+    cout << "Type your name:";
+    cin >> name;
+    cout << "Hello, " << name << endl;
     
     return 0;
 }
