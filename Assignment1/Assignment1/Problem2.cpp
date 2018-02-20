cc1f967f7955dd4fabcb80d87a00e93aa3bd9715
Stewart Dulaney <sdulaney@users.noreply.github.com>
2018-02-20 11:17:35 -0800

    add solution for problem 2

--- Assignment1/Assignment1/Problem2.cpp
+++ Assignment1/Assignment1/Problem2.cpp
@@ -7,7 +7,13 @@ using namespace std;
 
 int main()
 {
-    
+    double number1, number2, sum;
+    cout << "Please enter the first number:";
+    cin >> number1;
+    cout << "Please enter the second number:";
+    cin >> number2;
+    sum = number1 + number2;
+    cout << "The sum of " << number1 << " and " << number2 << " is " << sum << ".\n";
     
     return 0;
 }
