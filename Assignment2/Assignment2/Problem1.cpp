d16bac492ef98cf4b21e4d4df3ee80928d7cc797
Stewart Dulaney <sdulaney@users.noreply.github.com>
2018-02-28 01:17:17 -0800

    add solution for problem 1

--- Assignment2/Assignment2/Problem1.cpp
+++ Assignment2/Assignment2/Problem1.cpp
@@ -10,7 +10,26 @@ using namespace std;
 
 int main()
 {
-    
+    int num1, num2, num3, largest;
+    cout << "Enter an integer: ";
+    cin >> num1;
+    cout << "Enter an integer: ";
+    cin >> num2;
+    cout << "Enter an integer: ";
+    cin >> num3;
+    if ((num1 >= num2) && (num1 >= num3))
+    {
+        largest = num1;
+    }
+    else if ((num2 >= num1) && (num2 >= num3))
+    {
+        largest = num2;
+    }
+    else if ((num3 >= num1) && (num3 >= num2))
+    {
+        largest = num3;
+    }
+    cout << "The largest number is: " << largest << endl;
     
     return 0;
 }
