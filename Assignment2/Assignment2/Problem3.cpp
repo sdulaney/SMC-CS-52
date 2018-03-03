2c45d2f7f143022ca4ebcb4502ee84d3a02b4b23
Stewart Dulaney <sdulaney@users.noreply.github.com>
2018-03-03 13:21:46 -0800

    add solution for problem 3

--- Assignment2/Assignment2/Problem3.cpp
+++ Assignment2/Assignment2/Problem3.cpp
@@ -1,5 +1,5 @@
 // Stewart Dulaney
-// 2/28/2018
+// 3/3/2018
 // CS 52 Section 4110
 // SID: 1545566
 // Assignment 2
@@ -10,7 +10,31 @@ using namespace std;
 
 int main()
 {
-    
+    double input, sum = 0.0, average;
+    int count = 0;
+    do {
+        cout << "Number: ";
+        cin >> input;
+        if (input == -1.0)
+        {
+            break;
+        }
+        if (input < 0.0)
+        {
+            cout << "Invalid! Re-enter: ";
+            cin >> input;
+        }
+        if (input >= 0.0)
+        {
+            sum += input;
+            count++;
+        }
+    } while (input != -1.0);
+    cout.setf(ios::fixed);
+    cout.setf(ios::showpoint);
+    cout.precision(2);
+    average = sum / count;
+    cout << "Average: " << average << endl;
     
     return 0;
 }
