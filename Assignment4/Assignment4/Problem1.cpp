26f4ab11ec7298acbf34575e5fdacef0c79047ee
Stewart Dulaney <sdulaney@users.noreply.github.com>
2018-03-29 16:54:59 -0700

    add solution for problem 1

--- Assignment4/Assignment4/Problem1.cpp
+++ Assignment4/Assignment4/Problem1.cpp
@@ -5,11 +5,46 @@
 // Assignment 4
 // Problem 1
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
+int power(int x, int n);
+// Precondition: n is a nonnegative integer.
+// Returns x to the power n.
+
 int main()
 {
+    for (int i = 0; i <= 20; i++)
+    {
+        cout << "2 to the power " << i << " is equal to: " << power(2, i) << endl;
+    }
     
     return 0;
+}
+
+// Uses iostream, cstdlib:
+int power(int x, int n)
+{
+    if (n < 0)
+    {
+        cout << "Error! The argument n must be a nonnegative integer." << endl;
+        exit(1);
+    }
+    else if (n == 0)
+    {
+        return 1;
+    }
+    else if (n == 1)
+    {
+        return x;
+    }
+    else if ((n % 2) == 0)  // n is even
+    {
+        return (power(x, n / 2) * power(x, n / 2));
+    }
+    else                    // n is odd
+    {
+        return (power(x, n / 2) * power(x, n / 2) * x);
+    }
 }
