31f521fc09d85e48cf1d3f61a4950689182b8d97
Stewart Dulaney <sdulaney@users.noreply.github.com>
2018-04-20 16:20:20 -0700

    add solution for problem 2

--- Assignment5/Problem2.cpp
+++ Assignment5/Problem2.cpp
@@ -8,8 +8,40 @@
 
 using namespace std;
 
+int fac(int n);
+// Precondition: n >= 0 and n < 100.
+// Returns the value of n!.
+
 int main()
 {
+    int userInput, factorial;
+    cout << "Please enter a number: ";
+    cin >> userInput;
+    try {
+        factorial = fac(userInput);
+        cout << userInput << "! = " << factorial << endl;
+    } catch (const char* e) {
+        cout << e << endl;
+    }
     
     return 0;
+}
+
+int fac(int n)
+{
+    if (n < 0)
+    {
+        throw "Error! The parameter n must be a nonnegative integer.";
+    }
+    if (n >= 100)
+    {
+        throw "Error! The parameter n must be less than 100.";
+    }
+    // Base case
+    if (n == 0)
+    {
+        return 1;
+    }
+    // Recursive case
+    return (n * fac(n - 1));
 }
