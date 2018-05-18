9eb0577424a372f3b3cb0134c46029e7867152a0
Stewart Dulaney <sdulaney@users.noreply.github.com>
2018-05-18 03:44:25 -0700

    add circle class

---
+++ Assignment7/Assignment7/Circle.h
@@ -0,0 +1,31 @@
+// File: Circle.h
+// Stewart Dulaney
+// 5/18/2018
+// CS 52 Section 4110
+// SID: 1545566
+// Assignment 7
+// Problem 1
+
+#ifndef Circle_h
+#define Circle_h
+
+#include "Shape.h"
+
+class Circle : public Shape
+{
+private:
+    double radius;
+public:
+    Circle(double r);
+    Circle();
+    ~Circle();
+    double getRadius();
+    void setRadius(double radius);
+    double area();
+    // Returns the area of the circle.
+    void print();
+    // Prints the name and total area of the circle to
+    // the console.
+};
+
+#endif /* Circle_h */
