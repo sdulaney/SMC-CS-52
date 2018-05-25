81160e3a7a52bf9d1a47762ae3c2b70d334634ef
Stewart Dulaney <sdulaney@users.noreply.github.com>
2018-05-25 02:39:34 -0700

    add function getNaturalNumber

--- Assignment8/Assignment8/Problem1.cpp
+++ Assignment8/Assignment8/Problem1.cpp
@@ -8,9 +8,32 @@
 
 using namespace std;
 
+void getNaturalNumber(int& n);
+// Prompts the user to enter a positive integer until one is
+// entered. Sets n equal to the natural number value input by
+// the user.
+
 int main()
 {
-    
+    int n;
+    getNaturalNumber(n);
     
     return 0;
 }
+
+void getNaturalNumber(int& n)
+{
+    cout << "Input: ";
+    int userInput;
+    cin >> userInput;
+    do
+    {
+        if (userInput <= 0)
+        {
+            cout << "Input (>0): ";
+            cin >> userInput;
+        }
+    } while (userInput <= 0);
+    n = userInput;
+}
+
