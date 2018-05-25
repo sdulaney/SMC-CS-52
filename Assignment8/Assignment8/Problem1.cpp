fe295a7bd57209bb6a765c989acc7dc09f821171
Stewart Dulaney <sdulaney@users.noreply.github.com>
2018-05-25 02:48:40 -0700

    add function isPrime

--- Assignment8/Assignment8/Problem1.cpp
+++ Assignment8/Assignment8/Problem1.cpp
@@ -5,6 +5,7 @@
 // Assignment 8
 // Problem 1
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
@@ -12,6 +13,12 @@ void getNaturalNumber(int& n);
 // Prompts the user to enter a positive integer until one is
 // entered. Sets n equal to the natural number value input by
 // the user.
+
+bool isPrime(int n);
+// Precondition: n > 1. By definition an integer greater than one
+// is called a prime number if its only positive divisors (factors)
+// are one and itself.
+// Returns true if n is a prime number.
 
 int main()
 {
@@ -36,5 +43,23 @@ void getNaturalNumber(int& n)
         }
     } while (userInput <= 0);
     n = userInput;
+}
+
+// Uses iostream, cstdlib
+bool isPrime(int n)
+{
+    if (n <= 1)
+    {
+        cout << "Error! Parameter n must be greater than 1.\n";
+        exit(1);
+    }
+    for (int i = 2; i <= n / 2; i++)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
 }
 
